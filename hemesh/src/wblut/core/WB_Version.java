5256722977919de313d7c41fe03ad5aaa7372705
Frederik Vanhoutte (W:Blut) <frederikvanhoutte@192.168.0.198>
2014-07-12 02:17:37 +0200

    Updated version

--- hemesh/src/wblut/core/WB_Version.java
+++ hemesh/src/wblut/core/WB_Version.java
@@ -4,15 +4,15 @@ public class WB_Version {
 
 	public static final WB_Version CURRENT_VERSION = new WB_Version();
 
-	public static final int MAJOR = 1;
+	public static final int MAJOR = 2;
 
-	public static final int MINOR = 9;
+	public static final int MINOR = 0;
 
-	public static final int PATCH = 1;
+	public static final int PATCH = 0;
 
-	private static final String releaseInfo = "Balthasar";
+	private static final String releaseInfo = "Cardinal";
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		System.out.println(CURRENT_VERSION);
 	}
 
@@ -31,11 +31,14 @@ public class WB_Version {
 		return PATCH;
 	}
 
+	@Override
 	public String toString() {
-		String ver = "W:Blut HE_Mesh " + MAJOR + "." + MINOR + "." + PATCH;
-		if (releaseInfo != null && releaseInfo.length() > 0)
+		final String ver = "W:Blut HE_Mesh " + MAJOR + "." + MINOR + "."
+				+ PATCH;
+		if (releaseInfo != null && releaseInfo.length() > 0) {
 			return ver + " " + releaseInfo
 					+ System.getProperty("line.separator");
+		}
 		return ver;
 	}
 
